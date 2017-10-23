de3adc1f69e010586f2b1bd2a3d1ad16a67fa93f
raavr <rafal.r26@gmail.com>
2017-10-23 23:20:06 +0200

    fix bug: empty task title

--- src/app/tasks/tasks-list/to-array.filter.js
+++ src/app/tasks/tasks-list/to-array.filter.js
@@ -1,9 +1,7 @@
 'use strict';
 export default function ToArrayFilter() {
-	return function (obj, addKey) {
+	return function (obj) {
 		if (!(obj instanceof Object)) return obj;
-
-		if (!addKey) return Object.values(obj);
 
 		return Object.keys(obj).map(function (key) {
 			return Object.defineProperty(obj[key], '$key', { enumerable: false, value: key});
